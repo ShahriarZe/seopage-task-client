9446a21c584530fc48b92775e2365f8dad5ed9a1
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-10 23:49:03 +0600

    Horizontal & Vertical Scroll Bar Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,110 +3,292 @@ import './App.css'
 
 function App() {
 
-  
+
 
   return (
     <>
       <h1 className='text-center text-5xl font-extrabold'>Task Manager</h1>
 
       <div className=" mt-8 overflow-x-auto">
-      <div className="flex space-x-4">
-        <div className="w-1/4 bg-gray-200 p-4">
-          <h2 className="text-xl font-bold mb-4">To Do</h2>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 1 */}
-            <p>Task 1 - To Do</p>
+        <div className="flex space-x-4">
+          <div className="w-1/4 bg-gray-200 p-4 ">
+            <h2 className="text-xl font-bold mb-4">Incomplete</h2>
+
+            <div className="card-container h-[60vh] overflow-y-auto">
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - To Do</p>
+          <div className="w-1/4 bg-gray-200 p-4 ">
+            <h2 className="text-xl font-bold mb-4">TO DO</h2>
+
+            <div className="card-container h-[60vh] overflow-y-auto">
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - To Do</p>
+          <div className="w-1/4 bg-gray-200 p-4 ">
+            <h2 className="text-xl font-bold mb-4">Doing</h2>
+
+            <div className="card-container h-[60vh] overflow-y-auto">
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
-          <div className="bg-white p-4 rounded shadow w-72">
-            {/* Task Card 3 */}
-            <p>Task 3 - To Do</p>
+          <div className="w-1/4 bg-gray-200 p-4 ">
+            <h2 className="text-xl font-bold mb-4">Under Review</h2>
+
+            <div className="card-container h-[60vh] overflow-y-auto">
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
-        </div>
-        <div className="w-1/4 bg-gray-200 p-4">
-          <h2 className="text-xl font-bold mb-4">Ongoing</h2>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 1 */}
-            <p>Task 1 - Ongoing</p>
+          <div className="w-1/4 bg-gray-200 p-4 ">
+            <h2 className="text-xl font-bold mb-4">Completed</h2>
+
+            <div className="card-container h-[60vh] overflow-y-auto">
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - Ongoing</p>
-          </div>
-          <div className="bg-white p-4 rounded shadow w-72">
-            {/* Task Card 3 */}
-            <p>Task 3 - Ongoing</p>
-          </div>
-        </div>
-        <div className="w-1/4 bg-gray-200 p-4">
-          <h2 className="text-xl font-bold mb-4">Incomplete</h2>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 1 */}
-            <p>Task 1 - Incomplete</p>
-          </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - Incomplete</p>
-          </div>
-          <div className="bg-white p-4 rounded shadow w-72">
-            {/* Task Card 3 */}
-            <p>Task 3 - Incomplete</p>
-          </div>
-        </div>
-        <div className="w-1/4 bg-gray-200 p-4">
-          <h2 className="text-xl font-bold mb-4">Canceled</h2>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 1 */}
-            <p>Task 1 - Canceled</p>
-          </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - Canceled</p>
-          </div>
-          <div className="bg-white p-4 rounded shadow w-72">
-            {/* Task Card 3 */}
-            <p>Task 3 - Canceled</p>
-          </div>
-        </div>
-        <div className="w-1/4 bg-gray-200 p-4">
-          <h2 className="text-xl font-bold mb-4">Canceled</h2>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 1 */}
-            <p>Task 1 - Canceled</p>
-          </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - Canceled</p>
-          </div>
-          <div className="bg-white p-4 rounded shadow w-72">
-            {/* Task Card 3 */}
-            <p>Task 3 - Canceled</p>
-          </div>
-        </div>
-        <div className="w-1/4 bg-gray-200 p-4">
-          <h2 className="text-xl font-bold mb-4">Canceled</h2>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 1 */}
-            <p>Task 1 - Canceled</p>
-          </div>
-          <div className="bg-white p-4 mb-4 rounded shadow w-72">
-            {/* Task Card 2 */}
-            <p>Task 2 - Canceled</p>
-          </div>
-          <div className="bg-white p-4 rounded shadow w-72">
-            {/* Task Card 3 */}
-            <p>Task 3 - Canceled</p>
+          <div className="w-1/4 bg-gray-200 p-4 ">
+            <h2 className="text-xl font-bold mb-4">Canceled</h2>
+
+            <div className="card-container h-[60vh] overflow-y-auto">
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content mb-4">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            <div className="card w-80 bg-neutral text-neutral-content">
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Accept</button>
+                  <button className="btn btn-ghost">Deny</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
